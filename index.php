69af3abd029d44abd3a7077f67d4611234b36ad4
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-12-14 16:00:18 +0100

    Basic file set

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$title = 'Scioglilingua';
+$paragraph = 'Apelle figlio d\'Apollo fece una palla di pelle di pollo, tutti i pesci vennero a galla per vedere la palla di pelle di pollo fatta da Apelle figlio d\'Apollo.';
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Censuratore Badwords</title>
+  </head>
+  <body>
+    <h1>
+      <?php echo $title ?>
+    </h1>
+    <p>
+      <?php echo $paragraph
+      ?>
+    </p>
+  </body>
+</html>
